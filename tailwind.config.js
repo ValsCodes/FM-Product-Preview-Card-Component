677c676ed80b5e9d42386fc07cf07231b99b096e
Ivaylo Ivanov <ivailo1224@gmail.com>
2024-09-07 14:42:37 +0300

    Init commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xxs': '360px', // Extra extra small for small devices
+        'xss': '420px', // Another custom small size
+        // Keep the rest of your custom screen sizes here
+      },
+    },
   },
   plugins: [],
 }
